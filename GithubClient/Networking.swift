fa8f6d16943d01313f2f17a5b624180a5ca2ff5d
Henrik <henrik789@gmail.com>
2019-05-21 12:54:17 +0200

    init

---
+++ GithubClient/Networking.swift
@@ -0,0 +1,50 @@
+
+
+import Foundation
+
+
+
+class Networking {
+    
+    
+    func addRequest(_ request: Request, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: request.url){ (data, response, error) in
+            completion(data, response, error)
+//            print("Data: " , data, "Response: ",  response, "Error:",  error)
+        }
+        task.resume()
+    }
+    
+}
+
+enum Request {
+    case getUsers
+    case getImages
+    case getBigImages
+}
+
+extension Request {
+    var url: URL {
+        switch self {
+        case .getImages:
+            return URL(string: "https://api.github.com/users/henrik789")!
+        case .getBigImages:
+            return URL(string: "https://picsum.photos/400/?random")!
+        case .getUsers:
+            return URL(string: "https://jsonplaceholder.typicode.com/users")!
+        }
+    }
+}
+
+struct User: Codable{
+    var name: String = ""
+    var id: Int = 0
+    var public_repos: Int = 0
+    var bio: String = ""
+}
+
+//struct User: Codable {
+//
+//    let name: String
+//
+//}
--- GithubClient/ViewController.swift
+++ GithubClient/ViewController.swift
@@ -1,20 +1,115 @@
-//
-//  ViewController.swift
-//  GithubClient
-//
-//  Created by Henrik on 2019-05-20.
-//  Copyright © 2019 Henrik. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+private let badResponseError = NSError(domain: "Bad network response", code: 2, userInfo: nil)
 
+
+class ViewController: UIViewController {
+    
+    let networking = Networking()
+    private(set) var user = User()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //        updateUsers { (Error) in
+        //            print(self.users)
+        //        }
+        download()
+        
     }
-
-
+    
+    func download(){
+        guard let url = URL(string: "https://api.github.com/users/henrik789") else {return}
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let dataResponse = data,
+                error == nil else {
+                    print(error?.localizedDescription ?? "Response Error")
+                    return }
+            do{
+                //here dataResponse received from a network request
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                    dataResponse, options: [])
+                if let dictionary = jsonResponse as? [String: Any]{
+                    if let name = dictionary["name"] as? String{
+                        print("Name: " , name)
+                    }
+                }
+                let decoder = JSONDecoder()
+                let model = try decoder.decode(User.self, from:
+                    dataResponse) //Decode JSON Response Data
+                print(model)
+//                print(jsonResponse) //Response result
+            } catch let parsingError {
+                print("Error", parsingError)
+            }
+        }
+        task.resume()
+    }
+    
+    
+//    func download(){
+//        guard let url = URL(string: "https://api.github.com/users/henrik789") else {return}
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let dataResponse = data,
+//                error == nil else {
+//                    print(error?.localizedDescription ?? "Response Error")
+//                    return }
+//            do{
+//                //here dataResponse received from a network request
+//                let jsonResponse = try JSONSerialization.jsonObject(with:
+//                    dataResponse, options: [])
+//                if let dictionary = jsonResponse as? [String: Any]{
+//                    if let name = dictionary["name"] as? String{
+//                        print("Name: " , name)
+//                    }
+//                }
+//                print(jsonResponse) //Response result
+//            } catch let parsingError {
+//                print("Error", parsingError)
+//            }
+//        }
+//        task.resume()
+//    }
+    
+//    func updateUsers(completion: @escaping (Error?) -> Void) {
+//        getUsers { (users, error) in
+//            print("- -  checking - -")
+//            guard error == nil else {
+//                dispatchOnMain(completion, with: error)
+//                return
+//            }
+//            self.users = users
+//            dispatchOnMain(completion, with: nil)
+//        }
+//    }
+//    
+//    func getUsers(completion: @escaping ([User], Error?) -> Void) {
+//        networking.addRequest(.getImages) { (data, response, error)  in
+//            guard let data = data , error == nil else {
+//                let error = error ?? badResponseError
+//                completion([], error)
+//                return
+//            }
+//            
+//            do {
+//                let decoder = JSONDecoder()
+//                let users = try decoder.decode([User].self, from: data)
+//                completion(users, nil)
+//                print(users)
+//            } catch {
+//                completion([], error)
+//            }
+//        }
+//    }
+//    
+//    
+//}
+//
+//public func dispatchOnMain<T>(_ block: @escaping (T)->Void, with parameters: T) {
+//    DispatchQueue.main.async {
+//        block(parameters)
+//    }
 }
-
